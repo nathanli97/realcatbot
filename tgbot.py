7e512d8791b287c53f9f6263a9aec3f9f14eac62
Li Xingru <xingru97@gmail.com>
2023-12-10 21:40:27 +0800

    fix bug

--- tgbot.py
+++ tgbot.py
@@ -66,7 +66,9 @@ async def recv(update: Update, context: ContextTypes.DEFAULT_TYPE):
             if match:
                 action = match.group(1)
                 what = match.group(2)
-            await update.message.reply_text(f'{from_user_name}给{target_user_name}{action}{what}!')
+                await update.message.reply_text(f'{from_user_name}{target_user_name}{action}{what}!')
+            else:
+                await update.message.reply_text(f'{from_user_name}{action}{target_user_name}!')
 
 
 def main():
