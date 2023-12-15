39687640a301d0539a9f36d39cc23e3acbd81a2b
Li Xingru <xingru97@gmail.com>
2023-12-15 21:45:07 +0800

    支持皮套！（测试

--- plusminus.py
+++ plusminus.py
@@ -2,7 +2,7 @@ import logging
 import random
 import re
 
-from telegram import Update
+from telegram import Update, Message
 from telegram.ext import ContextTypes
 
 random_list = ["兔兔", "雁雁", "狗狗", "荧", "猫猫", "小企鹅", ]
@@ -12,6 +12,18 @@ random_list = ["兔兔", "雁雁", "狗狗", "荧", "猫猫", "小企鹅", ]
 #                        "想要传禁转资源并改官组后缀!", "想要去盗取他站界面!", "想要去开群友的盒!", "想要唱希望之花",
 #                        "想要手冲", "想去M-Team发自己的自制色情片","想要退群","想要被兔纸骂zako!","想要被兔纸暴打!",
 #                        "想要爬上兔纸的床!","想要去东京援交","想自觉地撅起屁股","想露出来给群友透","想要被后入"]
+def get_message_username(msg: Message) -> str:
+    user_name = msg.from_user.full_name
+    if msg.sender_chat and msg.sender_chat.title:
+        user_name = msg.sender_chat.title
+
+    if msg.from_user.username and msg.from_user.username == 'GroupAnonymousBot':
+        user_name = msg.chat.title
+
+        if msg.author_signature:
+            user_name += f' ({msg.author_signature})'
+    return user_name
+
 
 async def plus_or_minus(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message
@@ -19,7 +31,8 @@ async def plus_or_minus(update: Update, context: ContextTypes.DEFAULT_TYPE):
         logging.getLogger("Recv").warning("No reply to")
         return
 
-    reply_to_user = update.message.reply_to_message.from_user
+    reply_to_user_name = get_message_username(update.message.reply_to_message)
+    sender_user_name = get_message_username(update.message)
 
     text = msg.text[1:]
     match = re.fullmatch(r"(\d+)( .+)?", text)
@@ -36,6 +49,6 @@ async def plus_or_minus(update: Update, context: ContextTypes.DEFAULT_TYPE):
             action = "减"
 
         await update.message.reply_text(
-            f'{msg.from_user.full_name} 给 {reply_to_user.full_name} {action}了{num}{unit}!')
+            f'{sender_user_name} 给 {reply_to_user_name} {action}了{num}{unit}!')
     else:
         logging.getLogger("Recv").warning("REGEX match failed")
